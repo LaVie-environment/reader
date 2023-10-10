929fea80c19b8d786fac04b2eb3989c9a95c4147
armzig <bellotaophyc@outlook.com>
2023-10-10 22:32:15 +0100

    read the content of file

---
+++ README.md
@@ -0,0 +1,2 @@
+This fiddle of code reads the content of one or more files and print it to the console.
+
---
+++ tac.py
@@ -0,0 +1,26 @@
+import sys
+
+class CatCommand:
+    def __init__(self):
+        pass
+
+    def cat_files(self, file_paths):
+        for file_path in file_paths:
+            try:
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                    print(f"Content of {file_path}:\n{content}")
+            except FileNotFoundError:
+                print(f"Error: File '{file_path}' not found.")
+            except Exception as e:
+                print(f"Error reading file '{file_path}': {e}")
+
+if __name__ == "__main__":
+    cat_command = CatCommand()
+
+    # Check if file paths are provided as command-line arguments
+    if len(sys.argv) >= 1:
+        files_to_cat = sys.argv[1:]
+        cat_command.cat_files(files_to_cat)
+    else:
+        print("Usage: python cat_command.py [file1] [file2] ...")
